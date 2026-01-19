37be4653678481b898be2706156b6f14c9f10d59
Paulo Araújo <pharaujojr@gmail.com>
2026-01-19 09:52:05 -0400

    Add findByDescricao to ProdutoRepository

--- src/main/java/com/example/vendasjaragua/repository/ProdutoRepository.java
+++ src/main/java/com/example/vendasjaragua/repository/ProdutoRepository.java
@@ -4,6 +4,9 @@ import com.example.vendasjaragua.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Produto> findByDescricao(String descricao);
 }
